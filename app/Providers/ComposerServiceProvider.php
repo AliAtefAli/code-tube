bb678fe389d8093cb20cad64c8df7ada8dac1f20
AA <aliy.atef9@gmail.com>
2022-08-18 08:37:37 +0200

    setting up navigation bar

---
+++ app/Providers/ComposerServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use App\View\Composers\ChannelComposer;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+class ComposerServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        View::composer('layouts.navigation', ChannelComposer::class);
+    }
+}
